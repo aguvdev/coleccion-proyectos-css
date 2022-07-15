a1d919cf5e31503e654fbd5fe8b526686e299d38
aguvdev <hannheitiragustin@hotmail.com>
2022-07-15 02:27:07 -0300

    update: reduje el js y css

--- 29-slider-js-selection/main.js
+++ 29-slider-js-selection/main.js
@@ -80,9 +80,7 @@ function renderCurrentTitle(title, desc) {
 
 function renderImg() {
   const html = imagenes.map((image, index) => {
-    return `<img id="item" class="mini-img${index} ${
-      index == 0 ? "border" : ""
-    }" data-id=${index} src="./img/${image.img}" alt="">`;
+    return `<img id="item" class="mini-img ${index == 0 ? "border" : ""}" data-id=${index} src="./img/${image.img}" alt="">`;
   });
 
   imgContainer.innerHTML = html.join("");
@@ -105,41 +103,36 @@ function darBorde() {
   document.querySelectorAll("#item").forEach((item) => {
     minisIMAGE.push(item);
   });
-  const miniImg0 = minisIMAGE[0];
-  const miniImg1 = minisIMAGE[1];
-  const miniImg2 = minisIMAGE[2];
-  const miniImg3 = minisIMAGE[3];
-  const miniImg4 = minisIMAGE[4];
 
   if (current == 0) {
-    miniImg0.classList.add("border");
-    miniImg1.classList.remove("border");
-    miniImg2.classList.remove("border");
-    miniImg3.classList.remove("border");
-    miniImg4.classList.remove("border");
+    minisIMAGE[0].classList.add("border");
+    minisIMAGE[1].classList.remove("border");
+    minisIMAGE[2].classList.remove("border");
+    minisIMAGE[3].classList.remove("border");
+    minisIMAGE[4].classList.remove("border");
   } else if (current == 1) {
-    miniImg0.classList.remove("border");
-    miniImg1.classList.add("border");
-    miniImg2.classList.remove("border");
-    miniImg3.classList.remove("border");
-    miniImg4.classList.remove("border");
+    minisIMAGE[0].classList.remove("border");
+    minisIMAGE[1].classList.add("border");
+    minisIMAGE[2].classList.remove("border");
+    minisIMAGE[3].classList.remove("border");
+    minisIMAGE[4].classList.remove("border");
   } else if (current == 2) {
-    miniImg0.classList.remove("border");
-    miniImg1.classList.remove("border");
-    miniImg2.classList.add("border");
-    miniImg3.classList.remove("border");
-    miniImg4.classList.remove("border");
+    minisIMAGE[0].classList.remove("border");
+    minisIMAGE[1].classList.remove("border");
+    minisIMAGE[2].classList.add("border");
+    minisIMAGE[3].classList.remove("border");
+    minisIMAGE[4].classList.remove("border");
   } else if (current == 3) {
-    miniImg0.classList.remove("border");
-    miniImg1.classList.remove("border");
-    miniImg2.classList.remove("border");
-    miniImg3.classList.add("border");
-    miniImg4.classList.remove("border");
+    minisIMAGE[0].classList.remove("border");
+    minisIMAGE[1].classList.remove("border");
+    minisIMAGE[2].classList.remove("border");
+    minisIMAGE[3].classList.add("border");
+    minisIMAGE[4].classList.remove("border");
   } else if (current == 4) {
-    miniImg0.classList.remove("border");
-    miniImg1.classList.remove("border");
-    miniImg2.classList.remove("border");
-    miniImg3.classList.remove("border");
-    miniImg4.classList.add("border");
+    minisIMAGE[0].classList.remove("border");
+    minisIMAGE[1].classList.remove("border");
+    minisIMAGE[2].classList.remove("border");
+    minisIMAGE[3].classList.remove("border");
+    minisIMAGE[4].classList.add("border");
   }
 }
